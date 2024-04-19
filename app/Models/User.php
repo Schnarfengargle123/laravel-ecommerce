e39a9730625da9179f0bec450657db350bfbbb48
Schnarfengargle123 <schnarfengargle123@gmail.com>
2024-04-19 17:11:53 +0100

    Reinstalled  Admin Panel and modified  model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,49 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use CrudTrait;
     use HasFactory;
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'email',
+        'password',
+        'username',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+Route::get("/", function () {
+    return "Welcome to Fragrance Kabal";
+});
+
+Route::get("/users", function (Request $request) {
+    return User::all();
+});
+
+Route::get("/users/{id}", function (string $id) {
+    return User::find($id);
+});
